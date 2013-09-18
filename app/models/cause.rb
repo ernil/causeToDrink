98a35280dd57eba6f1aa3ff9895896f7809ce5d9
ernil <morion.ernil@gmail.com>
2013-09-18 10:17:47 +0400

    add validation

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,19 +1,23 @@
 class Cause < ActiveRecord::Base
   attr_accessible :approved, :author, :content, :country, :day, :month,:holiday
   
-  
-  #validates :holiday, inclusion: { in: %w(Birthday Religion National Other) }
-  #validates :content, presence: true, uniqueness: true
-  #validates :approved, inclusion: { in: [true, false]}
+  validates :holiday, inclusion: { in: %w(Birthday Religion National Other) }
+  validates :content, presence: true, uniqueness: true
+  validates :day, numericality: {
+    only_integer: true,
+    less_than_or_equal_to: 31
+  }
+  validates :month, numericality: {
+    only_integer: true,
+    less_than_or_equal_to: 12
+  }
 
-  #before_validation :set_prove
+  before_save :set_prove
 
-  #protected
-  # def set_prove
-  # 	if approved.nil?
-  # 		self.approved = false
-  # 	end
-  # end=end
+  def set_prove
+		self.approved = false if approved.nil?
+    true
+  end
 
   def self.find_random
     causes = Cause.where('(day = ? AND month = ?) OR (day IS ?)', Time.now.day, Time.now.month, nil)
